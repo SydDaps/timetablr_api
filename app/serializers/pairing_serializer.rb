8e8a2cd56e99926545fc4eb84c846a2da1c226b0
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-06-10 21:32:03 +0000

    ll

--- app/serializers/pairing_serializer.rb
+++ app/serializers/pairing_serializer.rb
@@ -2,14 +2,15 @@ class PairingSerializer < BaseSerializer
     def serialize_single(resource, _)
         {
             name: resource.course.name,
+            course: resource.course,
             start: resource.meet_time.start.strftime("%H:%M"),
             end: resource.meet_time.end.strftime("%H:%M"),
             lecturers:  LecturerSerializer.new( resource.course.lecturers ).serialize,
             room: resource.room.name,
             day: resource.day.number,
             time_tag: resource.time_tag.name,
-            department_code: resource.course.department,
-            level: resource.course.level
+            department: resource.course.department,
+            level: resource.course.level.code
         }
     end
 end
