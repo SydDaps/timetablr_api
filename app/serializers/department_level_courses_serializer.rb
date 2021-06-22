c8bb8536a38f7d370441c26f1312202df7d3ab9f
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-06-22 00:07:31 +0000

    updated serializer

--- app/serializers/department_level_courses_serializer.rb
+++ app/serializers/department_level_courses_serializer.rb
@@ -4,10 +4,9 @@ class DepartmentLevelCoursesSerializer < BaseSerializer
             department_name: resource.name,
             department_code: resource.code,
             levels: LevelSerializer.new( resource.time_table.levels.all ).serialize.map{
-                |l| { 
-                    code: l[:code],
+                |l| l.merge(
                     courses: CourseSerializer.new( Level.find(l[:id]).courses.where(department_id: resource.id ) ).serialize
-                }
+                ) 
             }
         }
     end
