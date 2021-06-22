29112480801b670d3bdfdd3c4baa111d92720905
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-06-22 20:34:27 +0000

    ff

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -6,8 +6,12 @@ class CourseSerializer < BaseSerializer
             code: resource.code,
             department: resource.department.code,
             type: resource.kind,
-            time_tags: TimeTagSerializer.new( resource.time_tags ).serialize,
-            lecturers: LecturerSerializer.new( resource.lecturers ).serialize,
+            time_tags: TimeTagSerializer.new( resource.time_tags ).serialize.map{
+                |tag| tag[:id]
+            },
+            lecturers: LecturerSerializer.new( resource.lecturers ).serialize.map{
+                |lecturer|  lecturer[:id]
+            },
             schedules: ScheduleSerializer.new( resource.course_schedules ).serialize
         }
 
