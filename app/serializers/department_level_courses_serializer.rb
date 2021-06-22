59e1b0d57ef33676fa716e585129045954a31de6
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-06-22 00:20:54 +0000

    updated serializer

--- app/serializers/department_level_courses_serializer.rb
+++ app/serializers/department_level_courses_serializer.rb
@@ -2,8 +2,8 @@ class DepartmentLevelCoursesSerializer < BaseSerializer
     def serialize_single(resource, _)
         {   
             department_id: resource.id,
-            department_name: resource.name,
-            department_code: resource.code,
+            name: resource.name,
+            code: resource.code,
             levels: LevelSerializer.new( resource.time_table.levels.all ).serialize.map{
                 |l| l.merge(
                     courses: CourseSerializer.new( Level.find(l[:id]).courses.where(department_id: resource.id ) ).serialize
