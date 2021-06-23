cdf8648e617ec5d09bfc2a1f41cdd0fd988daeed
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-06-23 00:40:56 +0000

    time shi

--- app/serializers/time_tag_serializer.rb
+++ app/serializers/time_tag_serializer.rb
@@ -1,9 +1,12 @@
 class TimeTagSerializer < BaseSerializer
     def serialize_single(resource, _)
+
+        duration_hours = Time.at( resource.duration ).utc.strftime("%H")
+        duration_minutes = Time.at( resource.duration ).utc.strftime("%M")
         {
             id: resource.id,
             name: resource.name,
-            duration: resource.duration,
+            duration: "#{duration_hours}hrs #{duration_minutes}mins" ,
             start_at: resource.start_at,
             end_at: resource.end_at
         }
