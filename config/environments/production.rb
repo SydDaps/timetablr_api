92ec69372586ed411e82243b4bd112da655f8912
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2021-09-16 20:57:28 +0000

    Timetable final push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,12 +116,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+  :address              => "smtp.sendgrid.net",
     :port                 => 587,
-    :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_PASSWORD"],
+    :user_name            => "apikey",
+    :domain               => 'timetablr.xyz',
+    :password             => ENV["API_KEY"],
     :authentication       => "plain",
     :enable_starttls_auto => true
-  }
 end
