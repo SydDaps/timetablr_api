cc3c18c0841f441c5907c16718fae53352e582aa
Sydney Dapilah <sdapilah@st.ug.edu.gh>
2022-03-24 23:09:59 +0000

    test

--- app/controllers/api/v1/scheduler_controller.rb
+++ app/controllers/api/v1/scheduler_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::SchedulerController < ApplicationController
 
 	def create
 		current_time_table.update!(status: "processing")
-		
+
 		ScheduleJob.perform_later(current_time_table)
-		
+
 		render json: {
 			success: true,
 			code: 200,
@@ -17,15 +17,16 @@ class Api::V1::SchedulerController < ApplicationController
 
 	def index
 
-		if current_time_table.status == "Pending" 
+		if current_time_table.status == "Pending"
 			raise Exceptions::UnauthorizedOperation.message("TimeTable still pending, make sure to generate it first")
 		end
-		
+
+    puts @current_user.name
 
 		schedule = current_time_table.schedules.all.map{ |p| PairingSerializer.new( p.pairings ).serialize }.flatten
 
 		if @current_user.class.name == "Student"
-			schedule = schedule.select do |l| 
+			schedule = schedule.select do |l|
 				l[:course][:level_id] == @current_user.level.id && l[:course][:department_id] == @current_user.department.id
 			end
 		elsif @current_user.class.name == "Lecturer"
@@ -36,11 +37,11 @@ class Api::V1::SchedulerController < ApplicationController
 				end
 
 			end
-			
+
 			schedule = temp
 		end
-		
-	
+
+
 
 		render json: {
 			success: true,
